932890c847ed5220c4a334fa3ea16ac7f1417b9a
Beau Smith <beau@beausmith.com>
2009-10-14 16:54:19 -0700

    adding css example

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
+# PNG Fix
+
 Allows the use of alpha transparent PNGs (both foreground and background) on MSIE 5.5-6. Works on elements that are loaded dynamically, as well as elements that use class names to toggle between different PNG backgrounds.
+
+Read more about [PNG Fix](http://www.pngfix.com/) hacks and solutions.
 
 ## Configuration
 
 To use, the following three variables need to be configured:
 
-1. transparentGIF - absolute path to the location of 1px x 1px transparent gif image.
+1. `transparentGIF` - absolute path to the location of 1px x 1px transparent gif image.
 
         transparentGIF: '/images/spacer.gif',
     
@@ -12,11 +16,19 @@ To use, the following three variables need to be configured:
     
         transparentGIF: 'http://domain.com/images/spacer.gif',
 
-2. foregroundPNGSelectors - comma separated CSS selectors (as you would specify in a css file) targeting elements using PNG in the foreground.
+2. `foregroundPNGSelectors` - comma separated CSS selectors (as you would specify in a css file) targeting elements using PNG in the foreground.
 
         foregroundPNGSelectors: ['img'],
 
-3. backgroundPNGSelectors - comma separated CSS selectors (as you would specify in a css file) targeting elements using PNG in the background.
+3. `backgroundPNGSelectors` - comma separated CSS selectors (as you would specify in a css file) targeting elements using PNG in the background.
+
+    When png images are specified as the background image for elements in the css:
+    
+        #header {background: url('/images/header-background.png')}
+        .comment {background: url('/images/comment-background.png')}
+        #nav li {background: url('/images/nav-item-background.png')}
+    
+    Entry the selectors in a comma-separated list like this:
 
         backgroundPNGSelectors: ['#header, .comment, #nav li'],
 
@@ -33,3 +45,5 @@ Note that background-position and background-repeat properties cannot be used, a
 However, this fix tries to mimic background-repeat by setting "sizingMethod" for the filter to "scale" if background-repeat is set to anything other than "no-repeat".
 
 Adapted from: <http://komodomedia.com/blog/index.php/2007/11/05/css-png-image-fix-for-ie>
+
+Copyright (c) 2008 Jason Cheow, under the MIT license
--- pngfix.js
+++ pngfix.js
@@ -1,45 +1,62 @@
 /*---------------------------------------------------------------------------
-Allows the use of alpha transparent PNGs (both foreground and background) on
-MSIE 5.5-6. Works on elements that are loaded dynamically, as well as
-elements that use class names to toggle between different PNG backgrounds.
+# PNG Fix
 
-To use, the following variables need to be configured:
+Allows the use of alpha transparent PNGs (both foreground and background) on MSIE 5.5-6. Works on elements that are loaded dynamically, as well as elements that use class names to toggle between different PNG backgrounds.
 
-  Location of 1px transparent gif (transparentGIF).
+Read more about [PNG Fix](http://www.pngfix.com/) hacks and solutions.
 
-  CSS selectors targeting elements using PNG in the foreground
-  (foregroundPNGSelectors).
+## Configuration
 
-  CSS selectors targeting elements using PNG in the background
-  (backgroundPNGSelectors).
+To use, the following three variables need to be configured:
 
-After configuration, include this file in the <head> portion of a page and
-limit the fix to IE 6 and below using conditional comments:
+1. `transparentGIF` - absolute path to the location of 1px x 1px transparent gif image.
 
-  <!--[if lte IE 6]>
-  <script type="text/javascript" src="/javascripts/pngfix.js">
-  <![endif]-->
+        transparentGIF: '/images/spacer.gif',
+    
+    or
+    
+        transparentGIF: 'http://domain.com/images/spacer.gif',
 
-Note that background-position and background-repeat properties cannot be
-used, as the AlphaImageLoader filter does not support positioning and
-tiling of images.
+2. `foregroundPNGSelectors` - comma separated CSS selectors (as you would specify in a css file) targeting elements using PNG in the foreground.
 
-However, this fix tries to mimic background-repeat by setting "sizingMethod"
-for the filter to "scale" if background-repeat is set to anything other
-than "no-repeat".
+        foregroundPNGSelectors: ['img'],
 
-Adapted from:
-http://komodomedia.com/blog/index.php/2007/11/05/css-png-image-fix-for-ie
+3. `backgroundPNGSelectors` - comma separated CSS selectors (as you would specify in a css file) targeting elements using PNG in the background.
+
+    When png images are specified as the background image for elements in the css:
+    
+        #header {background: url('/images/header-background.png')}
+        .comment {background: url('/images/comment-background.png')}
+        #nav li {background: url('/images/nav-item-background.png')}
+    
+    Entry the selectors in a comma-separated list like this:
+
+        backgroundPNGSelectors: ['#header, .comment, #nav li'],
+
+## Including Script
+
+After configuration, include this file in the <head> portion of a page and limit the fix to IE 6 and below using conditional comments:
+
+    <!--[if lte IE 6]>
+    <script type="text/javascript" src="/javascripts/pngfix.js">
+    <![endif]-->
+
+Note that background-position and background-repeat properties cannot be used, as the AlphaImageLoader filter does not support positioning and tiling of images.
+
+However, this fix tries to mimic background-repeat by setting "sizingMethod" for the filter to "scale" if background-repeat is set to anything other than "no-repeat".
+
+Adapted from: <http://komodomedia.com/blog/index.php/2007/11/05/css-png-image-fix-for-ie>
 
 Copyright (c) 2008 Jason Cheow, under the MIT license
 ---------------------------------------------------------------------------*/
 
 var PNGFix = {
-  transparentGIF: "/images/spacer.gif",
+
+  transparentGIF: '/images/spacer.gif',
 
   foregroundPNGSelectors: ['img'],
 
-  backgroundPNGSelectors: [],
+  backgroundPNGSelectors: [''],
 
   isPNGForeground: function(element) {
     return (element.src && element.src.match(/\.png/i))
